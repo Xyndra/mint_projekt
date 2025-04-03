cfa437be44a227596db53898e04bca3b6a181034
Xyndra <sammy@deutschergamingserver.de>
2025-04-03 13:28:49 +0200

    Replace Template

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,5 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/`
+  <img src="https://i.pinimg.com/736x/56/43/15/56431512d19c06582988282781d00aaa.jpg" alt="BG" />
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
