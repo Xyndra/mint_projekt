0f1a4c4f06d9775d017a13c9a6e29c990df93806
Xyndra <71836523+Xyndra@users.noreply.github.com>
2025-04-26 23:06:33 +0200

    make shinies possible

--- src/lib/PokeBox.svelte
+++ src/lib/PokeBox.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
     import Pokemon from "./Pokemon.svelte";
+
+    function range(start: number, end: number) {
+        return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    }
 </script>
 
 <div class="m-4 h-full">
     <h1 class="text-4xl text-nowrap">Pokemon Box</h1>
-    <div class="">
+    <div class="grid grid-cols-2">
         <Pokemon pokedex_number={1} />
+        <Pokemon pokedex_number={2} shiny={true} />
+        <Pokemon pokedex_number={1009} />
+        {#each range(18, 25) as id}
+            <Pokemon pokedex_number={id} />
+        {/each}
+        <Pokemon
+            pokedex_number={Math.round(Math.random() * 1025)}
+            shiny={true}
+        />
     </div>
 </div>
--- src/lib/Pokemon.svelte
+++ src/lib/Pokemon.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-    let { pokedex_number } = $props();
+    let { pokedex_number, shiny = false } = $props();
+    function genPath(pokedex_number: number, shiny: boolean): string {
+        let path = "/api/img/pokemon/";
+        if (shiny) {
+            path += "shiny/";
+        }
+        path += pokedex_number + ".png";
+        return path;
+    }
+    let path = $derived(genPath(pokedex_number, shiny));
+    function handleError(event: Event) {
+        console.error("Image loading error:", event);
+        pokedex_number = 0;
+    }
 </script>
 
-<img src={"/api/img/pokemon/" + pokedex_number + ".png"} alt="Pokemon" />
+<img src={path} onerror={handleError} alt="Pokemon" />
