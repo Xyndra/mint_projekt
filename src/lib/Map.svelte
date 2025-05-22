c427e178cdc7117088d99a62c5d7df5ddc7f2394
Xyndra <sammy@deutschergamingserver.de>
2025-05-22 16:26:26 +0200

    Fix Map.svelte and add some examples

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -25,13 +25,23 @@
         window.addEventListener("resize", updateSize);
         return () => {
             window.removeEventListener("resize", updateSize);
-    };
+        };
     });
 </script>
 
 <div id="outer" class="overflow-auto w-full h-full">
     <div class="relative">
-    <div style={`width: ${mapWidth}px; height: ${mapHeight}px;`}>
-        <img src={kanto} alt="Kanto Map" />
+        <div style={`width: ${mapWidth}px; height: ${mapHeight}px;`}>
+            <img src={kanto} alt="Kanto Map" class="inline" />
+        </div>
+        <div class="absolute top-0 left-0 pointer-events-none" style={`width: ${mapWidth}px; height: ${mapHeight}px;`}>
+            <MapPoint x={0} y={0} kind="catch" />
+            <MapPoint x={100} y={100} kind="event" />
+            <MapPoint x={200} y={200} kind="event" />
+            <MapPoint x={300} y={300} kind="catch" />
+            <MapPoint x={400} y={400} kind="city" city_name="Pewter City">
+                Test
+            </MapPoint>
+        </div>
     </div>
 </div>
